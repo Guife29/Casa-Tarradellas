54182d90d8dc5324234b5871aa1f21ccd2fbccba
Guillem <gfernandezlopez10@gmail.com>
2024-04-16 19:05:56 +0200

    a

--- JS/Scripts.js
+++ JS/Scripts.js
@@ -13,3 +13,17 @@ function hideCart(){
     // document.getElementById("menuDesplegable").style.display = "none";
     document.getElementById("cartDesplegable").style.right = "-75%";
 }
+
+// Carrousell de imagenes de recomendados
+
+const scrollers = document.querySelectorAll(".scroller");
+
+if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches){
+    addAnimation();
+}
+
+function addAnimation(){
+    scrollers.forEach((scroller) => {
+        scroller.setAttribute("data-animated", true);
+    });
+}
