7d22258f68697d24414f4f4f15fb49afd28eb189
Guillem <gfernandezlopez10@gmail.com>
2024-04-16 17:16:10 +0200

    a

---
+++ JS/Scripts.js
@@ -0,0 +1,16 @@
+function showCart(){
+    console.log("Enseño el menu");
+    // document.getElementById("menuDesplegable").style.display = "block";
+    document.getElementById("menuDesplegable").style.right = "0px";
+
+    var vaiable1 = 5;
+
+    console.log(vaiable1);
+}
+
+function hideCart(){
+    console.log("Escondo el menu");
+    // document.getElementById("menuDesplegable").style.display = "none";
+    document.getElementById("menuDesplegable").style.right = "-75%";
+
+}
